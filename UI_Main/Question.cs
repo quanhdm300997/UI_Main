f2039745979aa20f367182aae174e69e3b465372
quanhdm300997 <minhquan300997@gmail.com>
2019-02-21 23:13:27 +0700

    Add project files.

---
+++ UI_Main/Question.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI_Main
+{
+    class Question
+    {
+        String Title;
+        List<String> ChoiceContent;
+        
+        public Question()
+        {
+
+        }
+
+        public Question(String title, List<String> choice)
+        {
+            this.Title = title;
+            this.ChoiceContent = choice;
+        }
+    }
+}
